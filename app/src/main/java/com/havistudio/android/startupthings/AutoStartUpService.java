37f9ca4f3975cf8614121bbfbf4fa36bf0d94d82
Chaviaras Konstantinos <kostas@citystore.gr>
2017-08-03 19:36:25 +0300

    updates
    - add the Realm Mobile Database.

--- app/src/main/java/com/havistudio/android/startupthings/AutoStartUpService.java
+++ app/src/main/java/com/havistudio/android/startupthings/AutoStartUpService.java
@@ -5,11 +5,22 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
+import io.realm.Realm;
+
 /**
  * Created by kostas on 01/08/2017.
  */
 
 public class AutoStartUpService extends Service {
+
+    private static final String TAG = "AutoStartUpService";
+
+    public void onCreate(){
+        super.onCreate();
+        Realm.init(this);
+
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
--- app/src/main/java/com/havistudio/android/startupthings/MainActivity.java
+++ app/src/main/java/com/havistudio/android/startupthings/MainActivity.java
@@ -3,11 +3,14 @@ package com.havistudio.android.startupthings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.realm.Realm;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Realm.init(this);
         setContentView(R.layout.activity_main);
     }
 }
